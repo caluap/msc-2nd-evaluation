e3f4e66572645af105e50d6eb074c8bd927b69f3
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-02-26 22:01:15 -0300

    placeholders for data collection

--- src/views/Questionnaire.vue
+++ src/views/Questionnaire.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>QQQuest
-    <router-link to="/">Eval</router-link>
+  <div class="main-container">
+    <div class="text-container">
+      <h1>Nos fale um pouco sobre você</h1>
+      <p>Pegar formação</p>
+      <p>Pegar grupo de idade</p>
+      <router-link to="/eval" class="next-step">Começar teste</router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import "../assets/static/css/styles.scss";
 export default {
   name: "questionnaire"
 };
