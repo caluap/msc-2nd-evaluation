4cbc4aa082275e39a9a87fff409fe5c37f3aa2a1
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-04-04 16:52:43 -0300

    dummy data simmilar to the one firebase is spitting out

--- src/views/Summarizer.vue
+++ src/views/Summarizer.vue
@@ -6,10 +6,14 @@
 
 <script>
 import "../assets/static/css/styles.scss";
+import data from "../assets/static/data/dummy_data.json";
 export default {
   name: "summarizer",
   data() {
     return {};
+  },
+  mounted() {
+    console.log(data[0]);
   }
 };
 </script>
