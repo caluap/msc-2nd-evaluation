8278f01e38a4bf5d156db6c277c97c2886d58b88
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-05-27 22:22:03 -0300

    disable google analytics while i develop (and it's buggy anyway)

--- src/main.js
+++ src/main.js
@@ -7,16 +7,16 @@ import VueAnalytics from "vue-analytics";
 // turns off the 'You are running Vue in development mode.' msg
 Vue.config.productionTip = false;
 
-const isProd = process.env.NODE_ENV === "production";
-Vue.use(VueAnalytics, {
-  id: "UA-28336737-2",
-  router,
-  set: [{ field: "test_name", value: "data3" }],
-  debug: {
-    enabled: !isProd,
-    sendHitTask: isProd
-  }
-});
+// const isProd = process.env.NODE_ENV === "production";
+// Vue.use(VueAnalytics, {
+//   id: "UA-28336737-2",
+//   router,
+//   set: [{ field: "test_name", value: "data3" }],
+//   debug: {
+//     enabled: !isProd,
+//     sendHitTask: isProd
+//   }
+// });
 
 Vue.use(VueFirestore);
 
