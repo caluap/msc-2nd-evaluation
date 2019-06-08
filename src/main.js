589c4ea272ead1a2b3621e873fd664046d1a52b2
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-06-07 22:07:46 -0300

    definitely removing analytics module (the <head> code will suffice here)

--- src/main.js
+++ src/main.js
@@ -2,21 +2,9 @@ import Vue from "vue";
 import App from "./App";
 import VueFirestore from "vue-firestore";
 import router from "./router";
-import VueAnalytics from "vue-analytics";
 
 // turns off the 'You are running Vue in development mode.' msg
 Vue.config.productionTip = false;
-
-// const isProd = process.env.NODE_ENV === "production";
-// Vue.use(VueAnalytics, {
-//   id: "UA-28336737-2",
-//   router,
-//   set: [{ field: "test_name", value: "data3" }],
-//   debug: {
-//     enabled: !isProd,
-//     sendHitTask: isProd
-//   }
-// });
 
 Vue.use(VueFirestore);
 
