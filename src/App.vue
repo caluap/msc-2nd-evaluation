b38566389688054882c32360b0a90ac1d40b3284
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-06-17 15:44:49 -0300

    removed the buggy ip_find stuff and cleared it from the participants' data

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,6 @@
 
 <script>
 import "./assets/static/css/styles.scss";
-
-import { ip_find_key } from "./assets/js/keys.js";
 import { db } from "./firebase";
 import { firebaseApp } from "./firebase";
 import { general_data } from "./data";
@@ -35,23 +33,6 @@ export default {
               // this.author_id = user.user.uid;
               general_data.setAuthorId(user.user.uid);
               console.log("Hello, " + this.sharedState.author_id);
-
-              // let xmlhttp = new XMLHttpRequest();
-              // let ip_address = "179.159.57.90";
-
-              // let url =
-              //   "https://ipfind.co/?auth=" + ip_find_key + "&ip=" + ip_address;
-
-              // xmlhttp.onreadystatechange = function() {
-              //   if (this.readyState == 4 && this.status == 200) {
-              //     var result = JSON.parse(this.responseText);
-              //     db.collection("participants_data")
-              //       .doc(user.user.uid)
-              //       .set(result);
-              //   }
-              // };
-              // xmlhttp.open("GET", url, true);
-              // xmlhttp.send();
             },
             err => {
               console.log(err);
