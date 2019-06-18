2523395f03b48c8801c8d7df6714785b695f1200
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-06-18 10:44:15 -0300

    less impositive terms text

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -3,20 +3,20 @@
     <div class="text-container">
       <h1>Antes de começar...</h1>
       <p>
-        Antes de começar, precisamos que você leia e declare estar de acordo com nossos termos de privacidade. Você pode encontrá-los na íntegra
+        Esta é uma pesquisa homologada pelo
+        <a
+          href="https://prp-web.cenapad.unicamp.br/pt-br/cep-comite-de-etica-em-pesquisa"
+          target="_blank"
+        >Comitê de Ética em Pesquisa</a> (CEP) da Universidade Estadual de Campinas (Unicamp). O número de seu Certificado de Apresentação para Apreciação Ética (CAAE) é 09321819.1.0000.5404.
+      </p>
+      <p>
+        Ao participar desta avaliação, você deve declarar estar de acordo com nossos termos de privacidade. Você pode encontrá-los na íntegra
         <a
           href="/pdfs/tcle.pdf"
           target="_blank"
         >
           <strong>aqui</strong>
         </a>.
-      </p>
-      <p>
-        <br>Esta é uma pesquisa homologada pelo
-        <a
-          href="https://prp-web.cenapad.unicamp.br/pt-br/cep-comite-de-etica-em-pesquisa"
-          target="_blank"
-        >Comitê de Ética em Pesquisa</a> (CEP) da Universidade Estadual de Campinas (Unicamp). O número de seu Certificado de Apresentação para Apreciação Ética (CAAE) é 09321819.1.0000.5404.
       </p>
       <p>Importante ressaltar um ponto: se a qualquer momento e por qualquer razão você quiser abandonar o teste fique a vontade — não estará causando nenhum mal ao teste.</p>
       <p>
