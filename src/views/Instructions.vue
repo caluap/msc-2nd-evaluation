d02a463ff9a15611b77f9048ef9b554efd14047d
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-06-18 11:54:43 -0300

    changes instructions if there is of if there isn't a likert scale

--- src/views/Instructions.vue
+++ src/views/Instructions.vue
@@ -18,21 +18,34 @@
       </p>
       <img class="scheme" id="img1" src="@/assets/static/imgs/misc/scheme-1.svg">
       <img class="scheme" id="img2" src="@/assets/static/imgs/misc/scheme-2.svg">
-      <p>
-        Indique a seguir quão seguro você está de sua escolha
-        <span
-          class="img-identifier"
-          @mouseover="hoverImg('img3')"
-          @mouseleave="unhoverImg('img3')"
-        >3</span>. Quando tudo estiver pronto, clique no botão
-        <span
-          class="img-identifier"
-          @mouseover="hoverImg('img4')"
-          @mouseleave="unhoverImg('img4')"
-        >4</span> para avançar para a rodada seguinte.
-      </p>
-      <img class="scheme" id="img3" src="@/assets/static/imgs/misc/scheme-3.svg">
-      <img class="scheme" id="img4" src="@/assets/static/imgs/misc/scheme-4.svg">
+      <template v-if="likertLevels">
+        <p>
+          Indique a seguir quão seguro você está de sua escolha
+          <span
+            class="img-identifier"
+            @mouseover="hoverImg('img3')"
+            @mouseleave="unhoverImg('img3')"
+          >3</span>. Quando tudo estiver pronto, clique no botão
+          <span
+            class="img-identifier"
+            @mouseover="hoverImg('img4')"
+            @mouseleave="unhoverImg('img4')"
+          >4</span> para avançar para a rodada seguinte.
+        </p>
+        <img class="scheme" id="img3" src="@/assets/static/imgs/misc/scheme-3.svg">
+        <img class="scheme" id="img4" src="@/assets/static/imgs/misc/scheme-4.svg">
+      </template>
+      <template v-else>
+        <p>
+          Quando tudo estiver pronto, clique no botão
+          <span
+            class="img-identifier"
+            @mouseover="hoverImg('img3b')"
+            @mouseleave="unhoverImg('img3b')"
+          >3</span> para avançar para a rodada seguinte.
+        </p>
+        <img class="scheme" id="img3b" src="@/assets/static/imgs/misc/scheme-3.no-likert.svg">
+      </template>
       <p>Estimamos que o teste leve cerca de 10 minutos. Como usaremos o som do seu computador (ou celular), certifique-se de estar em um ambiente silencioso e que o barulho não vá incomodar ninguém.</p>
       <p>Se quiser fazer um teste, o tocador de áudio abaixo tem um exemplo semelhante aos que usaremos no teste.</p>
       <br>
@@ -54,6 +67,8 @@ export default {
   data() {
     return {
       sharedState: general_data.sharedState,
+      likertLevels:
+        general_data.data[general_data.sharedState.data].likertLevels,
       audio_file:
         general_data.data[general_data.sharedState.data].cards[0].audio
     };
